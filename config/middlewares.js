10c711297ba4d90ebf075f04d3e559073dafe185
hassancode1 <hassanjaeh@gmail.com>
2022-12-29 11:17:23 +0100

    middleware change

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'strapi.io', 'res.cloudinary.com/'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
